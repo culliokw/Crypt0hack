0fab6aeed584cce9c520077e96e0dbb3dd2ea95b
culliokw <26638519+culliokw@users.noreply.github.com>
2025-01-09 16:24:28 -0500

    Added Solution
    -Added solution of Structure_of_AES challenge

---
+++ Symmetric_Ciphers/Structure_of_AES/solution.py
@@ -0,0 +1,19 @@
+def bytes2matrix(text):
+    """ Converts a 16-byte array into a 4x4 matrix.  """
+    return [list(text[i:i+4]) for i in range(0, len(text), 4)]
+
+def matrix2bytes(matrix):
+    """ Converts a 4x4 matrix into a 16-byte array.  """
+    return [x for y in matrix for x in y]
+
+matrix = [
+    [99, 114, 121, 112],
+    [116, 111, 123, 105],
+    [110, 109, 97, 116],
+    [114, 105, 120, 125],
+]
+
+# Ensure our simple list comprehension to flatten our matrix works by print the result along with the potential flag
+print(matrix2bytes(matrix))
+print([chr(x) for x in matrix2bytes(matrix)])
+
